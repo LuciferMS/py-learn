7184e8f45c9fbd5cddad2806bbaaee3b06eaa596
LuciferMS <2039176261@qq.com>
2020-02-25 19:34:31 +0800

    python类型，语句，第一个python

---
+++ statement.py
@@ -0,0 +1,15 @@
+if True :
+    print("True");
+else :
+    print("False");
+
+
+a = 1;
+while a < 10:
+    print(a);
+    a += 2;
+
+
+animals = ["cats", "tigers", "dogs"];
+for a in animals:
+    print(a);
---
+++ type.py
@@ -0,0 +1,11 @@
+counter = 100;
+miles = 1000.0;
+name = "Elliot";
+print(counter);
+print(miles);
+print(name);
+
+a = b = c = 1;
+d, e, f = 1, 2, "john";
+
+del a, b, c
